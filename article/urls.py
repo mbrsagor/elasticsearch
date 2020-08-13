1485938c36601cfa18e90ca0513b0c0df5f91927
Sagor <mbrsagor@gmail.com>
2020-08-13 14:36:10 +0600

    Search static route add and implement bootstrap

---
+++ article/urls.py
@@ -0,0 +1,1 @@
+from django.urls import pathfrom .views import searchurlpatterns = [    path('', search, name='search'),]
--- article/views.py
+++ article/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def search(request):
+    context = {
+        'name': 'Mbr-Sagor'
+    }
+    template_name = 'search.html'
+    return render(request, template_name, context)
